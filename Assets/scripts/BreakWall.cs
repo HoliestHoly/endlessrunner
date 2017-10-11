638d8b57eb37bc1aedfce9dbe3f62c1ca25847dd
Joppe van Basten <21983@ma-web.nl>
2017-10-11 11:30:11 +0200

    Breakable walls added

---
+++ Assets/scripts/BreakWall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakWall : MonoBehaviour {
+
+
+    private PlayerStateEnum playerstateenum;
+
+    // Use this for initialization
+    void Start () {
+        playerstateenum = GetComponent<PlayerStateEnum>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var playerState = playerstateenum.playerState;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        var playerState = playerstateenum.playerState;
+        if (other.gameObject.tag == "BreakableWall" && playerState == PlayerStateEnum.PlayerStates.PlayerBall)
+        {
+            Debug.Log("ey");
+            Destroy(other.gameObject);
+        }
+    }
+
+}
